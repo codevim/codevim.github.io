563c0cfded8cec269efc419fb8684df7a97c0402
Ever <ieverx@outlook.com>
2018-03-04 23:00:10 +0800

    fix git

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,13 +6,12 @@ setup_git() {
 }
 
 commit_files() {
-    git checkout -b master
+    git init
     git add .
     git commit -m"Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 push() {
-    git init
     git remote add origin-pages https://${GH_TOKEN}@github.com/codevim/codevim.github.io.git 
     git push -f -u origin-pages master
 }
